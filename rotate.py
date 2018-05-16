8f4c291ec2cb7b2b4b91c07f7e8866423768b6c1
nilbarde <34839591+nilbarde@users.noreply.github.com>
2018-05-17 00:23:10 +0100

    Update rotate.py

--- rotate.py
+++ rotate.py
@@ -2,7 +2,9 @@ import cv2
 import numpy as np
 import os
 
-img = cv2.imread('1.png')
+name = '1.png'
+
+img = cv2.imread(name)
 rows , cols , _ = img.shape
 
 angle = 10 #counter clockwise positive
@@ -10,7 +12,7 @@ angle = 10 #counter clockwise positive
 M = cv2.getRotationMatrix2D((cols/2,rows/2),angle,1)
 img = cv2.warpAffine(img,M,(cols,rows))
 
-cv2.imwrite('1.png',img)
+cv2.imwrite(name,img)
 
 
 

17dab8bdb93460c083d902c701c3c754b9a9b24a
nilbarde <34839591+nilbarde@users.noreply.github.com>
2018-05-17 14:14:01 +0100

    rotate image
    read image and save after rotation, specified with different name

--- rotate.py
+++ rotate.py
@@ -2,17 +2,17 @@ import cv2
 import numpy as np
 import os
 
-name = '1.png'
-
-img = cv2.imread(name)
-rows , cols , _ = img.shape
-
+read_name = '1.png'
 angle = 10 #counter clockwise positive
+write_name = '2.png
+
+img = cv2.imread(read_name)
+rows , cols , _ = img.shape
 
 M = cv2.getRotationMatrix2D((cols/2,rows/2),angle,1)
 img = cv2.warpAffine(img,M,(cols,rows))
 
-cv2.imwrite(name,img)
+cv2.imwrite(write_name,img)
 
 
 

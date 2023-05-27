7d292900554dc6cfa76e41d536fef4e25a9b727e
nilbarde <nihalbarde@gmail.com>
2023-05-27 17:55:21 +0000

    os functions documented and improved

--- os_funs.py
+++ os_funs.py
@@ -1,77 +1,84 @@
-from os import walk, listdir, stat, makedirs
-from os.path import exists, getsize, isfile, getmtime, getctime, splitext, dirname
-from time import ctime
+import os
 from datetime import datetime
-import subprocess
 
-def get_folders(curr_folder,deep=False,address=False):
-	folders_com = []
-	for (dirpath, dirnames, filenames) in walk(curr_folder):
-		folders = []
-		if(address):
-			for fold in dirnames:
-				name = dirpath + '/' + fold + '/'
-				name = name.replace("//","/")
-				folders.append(name)
-		else:
-			folders = dirnames
-		folders.sort()
-		folders_com = folders_com + folders
-		if not deep:
-			break
-	return folders_com
 
-def get_files(folder,exts=None,address=False):
-	# files_fold = ([(i) for i in listdir(folder) if ("." in i)])
-	files_fold = []
-	for (dirpath, dirnames, filenames) in walk(folder):
-		files_fold = filenames
-		break
-	files_fold.sort()
-	files_ext = []
-	if not (exts is None):
-		if(type("s")==type(exts)):
-			exts = [exts]
-		for filename in files_fold:
-			for ext in exts:
-				if(filename.endswith(ext)):
-					files_ext.append(filename)
-	if(exts is None):
-		files_ext = files_fold
-	files = []
-	for filename in files_ext:
-		name = ''
-		if(address):
-			name += folder + "/"
-		name += filename
-		name = name.replace("//","/")
-		files.append(name)
-	return files
+def get_file_size(filepath):
+  """Gets the size of a file in bytes.
 
-def ensure_dir(file_path):
-	if '/' in file_path:
-		directory = dirname(file_path)
-		if not exists(directory):
-			makedirs(directory)
+  Args:
+    filepath: The path to the file.
 
-def get_size(file_path="."):
-	size_ = (subprocess.check_output(['du','-sh', file_path.replace("//","/")]).split()[0])
-	return str(size_),file_path
+  Returns:
+    The size of the file in bytes.
+  """
+  stat = os.stat(filepath)
+  return stat.st_size
 
-def get_modified_time(file_path):
-	ttt = getmtime(file_path)
-	edited = str(datetime.fromtimestamp(int(ttt)))
-	now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-	if(edited[:-9]==now[:-9]):
-		return(edited[-8:-3])
-	else:
-		if(edited[:4]==now[:4]):
-			return(edited[5:-9])
-		else:
-			return(edited[:4])
 
-def get_modified_date(file_path):
-	ttt = getmtime(file_path)
-	edited = str(datetime.fromtimestamp(int(ttt)))
-	now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-	return(edited[:-9])
+def get_files_in_directory(directory_path, suffix=None, recursive=False, return_full_path=False):
+    """
+    Retrieve files inside a directory, including sub-folders (if recursive is True).
+
+    Args:
+        directory_path (str): The path to the directory.
+        suffix (str, optional): A suffix to filter the file names. Defaults to None.
+        recursive (bool, optional): Whether to search for files recursively inside sub-folders. 
+            If set to True then return_full_path is also set to True. Defaults to False.
+        return_full_path (bool, optional): Whether to return the full file path or just the file name. Defaults to False.
+
+    Returns:
+        list: A list of file paths (or file names) matching the specified suffix (if provided) or all files in the directory.
+
+    Raises:
+        FileNotFoundError: If the specified directory does not exist.
+    """
+    if not os.path.exists(directory_path):
+        raise FileNotFoundError(f"The directory '{directory_path}' does not exist.")
+
+    return_full_path |= recursive
+    files = []
+    for root, _, filenames in os.walk(directory_path):
+        for file_name in filenames:
+            if suffix is None or file_name.endswith(suffix):
+                if return_full_path:
+                    files.append(os.path.join(root, file_name))
+                else:
+                    files.append(file_name)
+        if not recursive:
+            break
+
+    return files
+
+
+def get_last_modified_time(filepath):
+    """Gets the last modified time of a file.
+
+    Args:
+        filepath: The path to the file.
+
+    Returns:
+        The last modified time of the file as a datetime object.
+    """
+
+    return datetime.fromtimestamp(os.path.getmtime(filepath))
+
+
+def create_folder(folder_path):
+    """
+    Checks if the given folder exists or not, and if it does not exist, creates it.
+
+    Args:
+        folder_path (str): The path to the folder to check or create.
+
+    Returns:
+        bool: True if the folder exists or was created, False otherwise.
+    """
+
+    if not os.path.exists(folder_path):
+        try:
+            os.makedirs(folder_path)
+            return True
+        except:
+            return False
+    else:
+        return True

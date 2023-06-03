5f54047812748283ff5e8a648632d7452d9c48e9
nilbarde <nihalbarde@gmail.com>
2023-06-03 05:15:11 +0000

    optimized the code

--- myPool.py
+++ myPool.py
@@ -1,101 +1,58 @@
-from multiprocessing import Pool
+import multiprocessing
 import time
 from tqdm import tqdm
 
-
-def caller(args):
-    i, fun, args_call = args
-    return (i, fun(*args_call))
-
-
-class myPool(object):
-    def __init__(self, poolsize=4, max_time=6000, update_interval=1):
+class MultiprocessingTimer:
+    def __init__(self, target, args=(), kwargs=None, timeout=None, max_workers=None):
         """
-        Input:
-            poolsize:
-                multiprocessing number of threads
-            max_time:
-                maximum time required to execute all tasks (in seconds)
-                note:
-                    after time limit all working threads will be closed
-                    all incomplete nodes will return None
-            update_interval:
-                progress will be updated every (update_interval) seconds
+        Initialize the MultiprocessingTimer object.
+
+        Args:
+            target (function): The target function to be executed in parallel.
+            args (tuple): The arguments to be passed to the target function.
+            kwargs (list): The list of dictionaries, each representing keyword arguments for the corresponding task.
+            timeout (float): The maximum duration (in seconds) for the tasks to run.
+            max_workers (int): The maximum number of worker processes.
+
         """
-        self.poolsize = poolsize
-        self.pool = Pool(self.poolsize)
-        self.time_max = max_time
-        self.time_status = update_interval
+        self.target = target
+        self.args = args
+        self.kwargs = kwargs or [{}] * len(args)  # If kwargs is not provided, default to empty dictionary for each task
+        self.timeout = timeout
+        self.max_workers = max_workers
 
-    def callback(self, args):
+    def run(self):
         """
-        Internal function to store results
-        Do not call from outside
+        Execute the target function in parallel using multiprocessing.
+
+        Returns:
+            list: A list of tuples containing the original arguments and the corresponding results.
+
         """
-        i, res = args
-        self.results[i] = res
+        results = []
+        completed_tasks = 0
 
-    def start(self, fun=None, args_list=[]):
-        """
-        Function to be called
-        Inputs:
-            fun:
-                function to be called
-            args_list:
-                list of arguments to be passed to fun
-                note:
-                    args_list should be list of tuple
-                    tuple can contain multiple arguments
-        """
-        self.fun = fun
-        self.args_list = args_list
+        with multiprocessing.Pool(processes=self.max_workers) as pool:
+            # Submit tasks to the pool
+            tasks = [pool.apply_async(self.target, arg, kwarg) for arg, kwarg in zip(self.args, self.kwargs)]
 
-        self.calls = []
-        self.results = [None for _ in range(len(self.args_list))]
-        for i, args in enumerate(self.args_list):
-            call = self.pool.apply_async(
-                caller,
-                args=((i, self.fun, args), ),
-                callback=self.callback
-            )
-            self.calls.append(call)
+            # Create a progress bar
+            with tqdm(total=len(tasks), desc="Tasks completed") as pbar:
+                start_time = time.time()
+                while time.time() - start_time < self.timeout:
+                    time.sleep(1)
+                    completed_tasks = sum(task.ready() for task in tasks)
+                    pbar.update(completed_tasks - pbar.n)
 
-        self.time_start = time.time()
-        self.total_threads = len(self.args_list)
-        time_spent = 0
-        self.progress = tqdm(total=self.total_threads, desc="Pool", position=0)
+                # Terminate the remaining tasks
+                pool.terminate()
 
-        while (time_spent < self.time_max) and self.calls:
-            time.sleep(self.time_status)
-            remaining_calls = []
-            for call in self.calls:
-                try:
-                    call.successful()
-                except:
-                    remaining_calls.append(call)
-            time_now = time.time()
-            time_spent = time_now - self.time_start
-            self.progress.update(len(self.calls) - len(remaining_calls))
-            self.calls = remaining_calls[:]
+                # Collect the results
+                for task, arg, kwarg in zip(tasks, self.args, self.kwargs):
+                    if task.ready():
+                        result = task.get()
+                        results.append((arg, result))
+                    else:
+                        results.append((arg, None))
 
-        self.pool.terminate()
-        self.pool.close()
-        self.pool.join()
-        res = self.results
-
-        self.progress.close()
-        self.clear_memory()
-
-        return res
-
-    def clear_memory(self):
-        del self.results
-        del self.calls
-        del self.progress
-        del self.total_threads
-        del self.time_start
-        del self.time_max
-        del self.time_status
-        del self.fun
-        del self.args_list
-        del self.poolsize
+        return results
